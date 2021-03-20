ab013816056b74477f6f90cd2425644498f03a34
R4WM4T <81012587+R4WM4T@users.noreply.github.com>
2021-03-20 11:03:47 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+Ce script python permet de crypter et décrypter une chaîne de caractères par l'intermédiaire du code César.
+
+Cela consiste à remplacer chacune des lettres par une autre en fonction du "décalage" préalablement choisi.
+Par Exemple, lorsque la lettre "a" subit un décalage de 3, elle devient alors la lettre "d".
+Il faut par ailleurs reprendre au début de l'alphabet lorsque le décalage appliqué à une lettre est plus important que la 
+longueur de l'alphabet.
+
+Ce script permet aussi de décrypter un message codé à l'aide du code césar en soustrayant cette fois le décalage à la 
+lettre. Cependant il est nécessaire de posséder la valeur du décalage (le test par la force-brute étant trop gourmand en 
+ressources).
+
+J'ai effectué ce script car j'apprécie étudier les différentes méthodes d'encryptage, notamment celles qui ont traversé 
+l'histoire.
---
+++ codeCesar.py
@@ -0,0 +1,24 @@
+﻿alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+decalage = int(input("Entrez le décalage:"))
+mot = str(input("Entrez la chaîne de caractère:")).lower()
+mot_code = []
+mot_decode = []
+
+for everyLetter in mot:
+    extremum = 0
+    if everyLetter in alphabet:
+        if alphabet.index(everyLetter)+decalage > len(alphabet):
+            extremum = len(alphabet)
+        mot_code.append(alphabet[alphabet.index(everyLetter)+decalage-extremum])
+        extremum = 0
+        if alphabet.index(everyLetter)-decalage < 0:
+            extremum = len(alphabet)
+        mot_decode.append(alphabet[alphabet.index(everyLetter)-decalage+extremum])
+    else:
+        mot_code.append(everyLetter)
+        mot_decode.append(everyLetter)
+
+
+print("Votre chaîne de caractère codée:"+''.join(mot_code))
+print("Votre chaîne de caractère décodée:"+''.join(mot_decode))
+
